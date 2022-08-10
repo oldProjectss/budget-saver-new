0cd70cd69d4b9a9a2bb6c53adee0fc090f6e163a
Ismail Courr <contact@ismailcourr.ml>
2022-08-10 14:41:02 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'splash#index'
-  get '/users/:id', to: 'groups#index', as: 'user_root'
+  # get '/users/:id', to: 'groups#index', as: 'user_root'
   resources :groups do
     resources :conversions
     resources :group_conversions
