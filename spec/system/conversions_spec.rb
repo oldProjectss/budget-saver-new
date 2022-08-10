cd0a77063d238b69adebdcd1ec1226076bd2a4b7
Ismail Courr <contact@ismailcourr.ml>
2022-08-10 17:01:31 +0100

    Fix tests

--- spec/system/conversions_spec.rb
+++ spec/system/conversions_spec.rb
@@ -3,94 +3,50 @@ require 'rails_helper'
 RSpec.describe 'Conversions', type: :system do
   before do
     driven_by(:rack_test)
+
+    visit '/users/sign_up'
+
+    fill_in 'Full name', with: 'ismail courr'
+    fill_in 'Email', with: 'test@email.com'
+    fill_in 'Password', with: 'test1234'
+    fill_in 'Confirm password', with: 'test1234'
+    click_on 'Next'
+    visit '/'
   end
 
   describe 'Conversions' do
-    it 'should create a conversion' do
-      visit conversions_url
-      click_on 'New conversion'
+    it 'should create a conversion (transaction) to the app' do
+      click_on 'Add a new category'
+      fill_in 'icon url e.g: https://images.com/pic.png', with: 'https://images.com/pic.png'
+      fill_in 'Category name', with: 'Amazon'
+      click_on 'Create Group'
+      click_on 'Amazon'
+      click_on 'Add a new transaction'
+      fill_in 'Amount', with: '100'
+      fill_in 'Name', with: 'Kendel'
+      click_on 'Create Transaction'
 
-      fill_in 'User', with: 1
-      fill_in 'Amount', with: 1
-      fill_in 'Authorid', with: 1
-      fill_in 'Name', with: 'test'
-      click_on 'Create Conversion'
-
-      expect(page).to have_content 'Conversion was successfully created'
-      click_on 'Back'
+      expect(page).to have_content 'Transaction was successfully created'
+      expect(page).to have_content 'Amazon'
     end
 
-    it 'should update a conversion' do
-      visit conversion_url(1)
-      click_on 'Edit this conversion', match: :first
+    it 'should add a conversion (transaction) to a Group' do
+      click_on 'Add a new category'
+      fill_in 'icon url e.g: https://images.com/pic.png', with: 'https://images.com/pic.png'
+      fill_in 'Category name', with: 'Amazon'
+      click_on 'Create Group'
+      click_on 'Amazon'
+      click_on 'Add a new transaction'
+      fill_in 'Amount', with: '100'
+      fill_in 'Name', with: 'Kendel'
+      click_on 'Create Transaction'
+      click_on 'Add transaction to this group'
 
-      fill_in 'User', with: 1
-      fill_in 'Amount', with: 1
-      fill_in 'Authorid', with: 1
-      fill_in 'Name', with: 'test'
-      click_on 'Update Conversion'
-
-      expect(page).to have_content 'Conversion was successfully updated'
-      click_on 'Back'
-    end
-
-    it 'should destroy a conversion' do
-      visit conversion_url(1)
-      click_on 'Destroy this conversion', match: :first
-
-      expect(page).to have_content 'Conversion was successfully destroyed'
-    end
-
-    it 'should not create a conversion without a name' do
-      visit conversions_url
-      click_on 'New conversion'
-
-      fill_in 'User', with: 1
-      fill_in 'Amount', with: 1
-      fill_in 'Authorid', with: 1
-      click_on 'Create Conversion'
-
-      expect(page).to have_content "Name can't be blank"
-      click_on 'Back'
-    end
-
-    it 'should not create a conversion without a user' do
-      visit conversions_url
-      click_on 'New conversion'
-
-      fill_in 'Amount', with: 1
-      fill_in 'Authorid', with: 1
-      fill_in 'Name', with: 'test'
-      click_on 'Create Conversion'
-
-      expect(page).to have_content "User can't be blank"
-      click_on 'Back'
-    end
-
-    it 'should not create a conversion without an amount' do
-      visit conversions_url
-      click_on 'New conversion'
-
-      fill_in 'User', with: 1
-      fill_in 'Authorid', with: 1
-      fill_in 'Name', with: 'test'
-      click_on 'Create Conversion'
-
-      expect(page).to have_content "Amount can't be blank"
-      click_on 'Back'
-    end
-
-    it 'should not create a conversion without an author' do
-      visit conversions_url
-      click_on 'New conversion'
-
-      fill_in 'User', with: 1
-      fill_in 'Amount', with: 1
-      fill_in 'Name', with: 'test'
-      click_on 'Create Conversion'
-
-      expect(page).to have_content "Authorid can't be blank"
-      click_on 'Back'
+      expect(page).to have_content 'Transaction was successfully added to group'
+      expect(page).to have_content 'Amazon'
+      expect(page).to have_content 'Total Transactions: $100.0'
+      expect(page).to have_content 'Kendel'
+      expect(page).to have_content 'Transaction N: '
     end
   end
 end
